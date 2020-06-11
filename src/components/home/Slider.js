7d6cad123ace7dec8c8e01779f73cfaf860602c4
Sivanat S <sivanathsubramani@gmail.com>
2020-06-11 18:36:31 +0530

    refs #Slider Design Commit.

---
+++ src/components/home/Slider.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import AnimatedSlider from "react-animated-slider";
+import "react-animated-slider/build/horizontal.css";
+import "./Slider.css";
+import home_1 from "../../assets/veera_home_1.jpg";
+import home_2 from "../../assets/veera_home_2.jpg";
+
+const content = [
+  {
+    title: "Vulputate Mollis Ultricies Fermentum Parturient",
+    description:
+      "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.",
+    button: "Learn More",
+    image: home_1,
+  },
+  {
+    title: "volutpat Aenean metus",
+    description:
+      "quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentumconsectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.",
+    button: "Learn More",
+    image: home_2,
+  },
+  {
+    title: "Vulputate Mollis Ultricies Fermentum Parturient",
+    description:
+      "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.",
+    button: "Learn More",
+    image: home_1,
+  },
+];
+
+class Slider extends Component {
+  render() {
+    return (
+      <AnimatedSlider autoplay={3000}>
+        {content.map((item, index) => (
+          <div
+            style={{
+              backgroundImage: `url(${item.image})`,
+              backgroundPosition: `center`,
+              minHeight: `800px`,
+            }}
+          >
+            <div class="slider-content">
+              <div class="slider-inner">
+                <div class="slider-header">
+                  {/* <h1>{item.title}</h1> */}
+                  {item.title}
+                </div>
+                <div class="slider-para">
+                  {/* <p>{item.description}</p> */}
+                  {item.description}
+                </div>
+                <div class="slider-button">
+                  {/* <button>{item.button}</button> */}
+                  {item.button}
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </AnimatedSlider>
+    );
+  }
+}
+
+export default Slider;
--- src/container/home/Home.js
+++ src/container/home/Home.js
@@ -1,36 +1,11 @@
 import React, { Component } from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "./Slider.css";
-
+import Slider from "../../components/home/Slider";
+// import AnimatedSlider from "../../components/home/animated-slider/AnimatedSlider";
 class Home extends Component {
   render() {
-    var settings = {
-      dots: false,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: true,
-      className: "slides",
-    };
     return (
       <div>
-        <Slider {...settings}>
-          <div>
-            <img
-              width="100%"
-              src="https://image3.jdomni.in/library/06032020/D5/E7/15/814322DC22BD86B0623C021247_1583498931048.webp"
-            />
-          </div>
-          <div>
-            <img
-              width="100%"
-              src="https://image2.jdomni.in/library/06032020/D7/60/BB/C2FE3D840FD20F8CFE8B8E2819_1583498932847.webp"
-            />
-          </div>
-        </Slider>
+        <Slider></Slider>
       </div>
     );
   }
